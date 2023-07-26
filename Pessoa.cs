2f18aeb592ea6f11ff856f52c6993ae095a524f1
Wendell Mickael Cardoso <135742936+Oiquerida69@users.noreply.github.com>
2023-07-26 15:26:20 -0300

    Add files via upload

---
+++ Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula13ClassesPOO
+{
+    internal class Pessoa
+    {
+        //listas
+
+       
+
+        //Variaveis (atributos)
+        public string nome;
+        public int idade;
+        public string rua;
+        public string cidade;
+
+        //Método construtor
+        public Pessoa(string nome , int idade , string rua , string cidade )
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.rua = rua;
+            this.cidade = cidade;
+            
+
+        }
+
+        //Funções (Métodos)
+
+        public void mostra()
+        {
+           
+            Console.WriteLine($"Olá, o seu nome é {nome} , você tem {idade} e mora na {rua} , cidade de {cidade}");
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Aula13ClassesPOO;
+
+
+
+Pessoa Wendell = new Pessoa("Wendell" ,22 , "João martins frança" , "São carlos");
+Wendell.mostra();
+Pessoa Mirela = new Pessoa("Mirela", 18, "rua vicente laurito", "São carlos");
+Mirela.mostra();
+Pessoa marcelly = new Pessoa("Marcelly", 25, "Attillio Milanetto", "São carlos");
+marcelly.mostra();
+List<Pessoa> pessoas = new List<Pessoa>();
+pessoas.Add(Wendell);
+pessoas.Add(Mirela);
+pessoas.Add(marcelly);
+
+for (int i = 0; i < pessoas.Count; i++)
+{
+    pessoas[i].mostra();
+    // o comando acima mostra cada uma das instancias em ordem junto com a função mostra
+
+}
